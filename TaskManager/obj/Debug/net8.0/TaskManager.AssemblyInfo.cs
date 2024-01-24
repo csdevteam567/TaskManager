f337ffa5f4db247a5537754ffe56fd92762387cf
Dmitriy Mikhalskiy <csdevteam@rambler.ru>
2024-01-24 17:02:29 +0200

    commit 4

--- TaskManager/obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8ddba680f131dd138d06baaaf9415f2eacba61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0f7e0e093f574e19c55934c99a6fb7d850dac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
